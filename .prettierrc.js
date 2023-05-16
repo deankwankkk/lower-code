4efb4512b43957a1babcd02d9c9ee2712a93f485
dean.kwan <deankwan@163.com>
2023-05-16 11:04:59 +0800

    chore: init

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  // 箭头函数只有一个参数的时候，可以忽略括号
+  arrowParens: 'avoid',
+  // 括号内部不要出现空格
+  bracketSpacing: true,
+  // 行结束符使用 Unix格式
+  endOfLine: 'lf',
+  jsxBracketSameLine: false,
+  printWidth: 100,
+  proseWrap: 'preserve',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: true,
+  trailingComma: 'es5',
+  parser: 'typescript',
+}
